b92216998589a3288d11d58fba4e98a9e2ca9fc6
hkrilham <hkr.ilham@gmail.com>
2024-08-12 13:45:23 +0530

    dart collections

---
+++ bin/collectionss.dart
@@ -0,0 +1,15 @@
+void main() {
+  List list = ['ilham', 26, 'srilanka', true, 5.10];
+  print(list);
+
+  Map map = {
+    'name': 'ilham',
+    'age': 26,
+    'address': 'trincomalee',
+    'more': Null
+  };
+  print(map);
+
+  Set weekDays = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+  print(weekDays);
+}
