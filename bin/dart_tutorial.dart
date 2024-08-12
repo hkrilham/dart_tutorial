cf920a5f01cab93211fb0a127755202adc41d3f4
hkrilham <hkr.ilham@gmail.com>
2024-08-12 12:53:14 +0530

    print functionce prectis

--- bin/dart_tutorial.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_tutorial/dart_tutorial.dart' as dart_tutorial;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_tutorial.calculate()}!');
-}
---
+++ bin/print_functions.dart
@@ -0,0 +1,8 @@
+void main() {
+  print('Hello world');
+
+  String world = 'hello world';
+  print(world);
+  int number = 29;
+  print(number);
+}
