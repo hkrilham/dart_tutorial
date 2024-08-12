7d3ee008edd79223ada20df1408d6ea1a45989b9
hkrilham <hkr.ilham@gmail.com>
2024-08-12 13:29:36 +0530

    learn variables typs

--- bin/print_functions.dart
+++ bin/print_functions.dart
@@ -1,8 +1,20 @@
 void main() {
+  //  String printing
   print('Hello world');
-
   String world = 'hello world';
   print(world);
+
+  // int printing
   int number = 29;
   print(number);
+
+//print double
+  double no = 3.18;
+  print(no);
+
+// num (int , double)
+  num no1 = 10;
+  num no2 = 4.444;
+  print(no1);
+  print(no2);
 }
---
+++ bin/variables.dart
@@ -0,0 +1,24 @@
+// dart Variables
+//Type of dart variables
+// String
+// int
+// double
+// num
+// bool
+// var
+
+void main() {
+  String name = 'ilham';
+  int age = 26;
+  double height = 5.10;
+  num balence = 100000000.4;
+  bool isMarid = true;
+  var anyThing = 'hello world';
+
+  print(name);
+  print(age);
+  print(height);
+  print(balence);
+  print(isMarid);
+  print(anyThing);
+}
